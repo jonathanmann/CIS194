017d7591b93cc9ef0eba4ab1be4d529a8e64a68e
Jonathan Mann <mann17@gmail.com>
2016-11-30 21:47:59 -0500

    add .gitignore

---
+++ week1/problem1.hs
@@ -0,0 +1,78 @@
+{-# OPTIONS_GHC -Wall #-}
+
+-- Exercise 1
+
+-- toDigits converts an integer to a list of its compenent digits
+toDigits :: Integer -> [Integer]
+toDigits x
+    | x <=0 = []
+    | otherwise = toDigits (x `div` 10) ++ [x `mod` 10]
+
+
+-- toDigitsRev converts an integer to a reversed list of its compenent digits
+toDigitsRev :: Integer -> [Integer]
+toDigitsRev = reverse . toDigits 
+
+{-
+-- longer version
+toDigitsRev :: Integer -> [Integer]
+toDigitsRev x 
+    | x <=0 = []
+    | otherwise = x `mod` 10 : toDigitsRev (x `div` 10)
+-}
+
+
+-- Exercise 2
+
+-- doubleEveryOther doubles every other element in the list
+-- starting from the penultimate item and then working backward
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther = reverse . zipWith (*) p . reverse
+    where p = 1:2:p
+
+-- Exercise 3
+
+-- sumDigits 
+sumDigits :: [Integer] -> Integer
+sumDigits (x:xs) = sum (toDigits x) + sumDigits xs
+sumDigits _ = 0
+-- sumDigits xs = sum $ map (sum . toDigits) xs
+
+-- Exercise 4
+
+-- validate
+validate :: Integer -> Bool
+validate x = sumDigits(doubleEveryOther(toDigits x)) `mod` 10 == 0
+
+-- Run tests
+main :: IO ()
+main = do
+
+    print "Expecting [1,2,3,4]"
+    print (toDigits 1234)
+    print (toDigits 1234 == [1,2,3,4])
+
+    print "Expecting [4,3,2,1]"
+    print (toDigitsRev 1234)
+    print (toDigitsRev 1234 == [4,3,2,1])
+
+    print "Expecting [16,7,12,5]"
+    print (doubleEveryOther [8,7,6,5])
+    print (doubleEveryOther [8,7,6,5] == [16,7,12,5])
+
+    print "Expecting [1,4,3]"
+    print (doubleEveryOther [1,2,3])
+    print (doubleEveryOther [1,2,3] == [1,4,3])
+    
+    print "Expecting 22"
+    print (sumDigits [16,7,12,5])
+    print (sumDigits [16,7,12,5] == 22)
+
+    print "Expecting True"
+    print (validate  4012888888881881)
+    print (validate  4012888888881881 == True)
+
+    print "Expecting False"
+    print (validate  4012888888881882)
+    print (validate  4012888888881882 == False)
+
